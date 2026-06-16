b5e3024924f29606837769ac8e16205d53bdfc5e
asliddinilhomjonov3-a11y <asliddinilhomjonov3@gmail.com>
2026-06-16 15:59:22 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# HTML fayllar turgan papkani ulash
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-@app.post("/create-video/")
-async def create_ai_video(prompt: str, user_id: str):
-    # Bu yerda video yasash mantig'i bo'ladi
-    return {"message": f"Video tayyorlanmoqda: {prompt}", "status": "processing"}
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-
-app = FastAPI()
-
-# HTML fayllar turgan papkani ko'rsatish
+# HTML fayllar uchun yo'l
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
