996067fc27e021a859823c92e69bf84f8925b1fe
asliddinilhomjonov3-a11y <asliddinilhomjonov3@gmail.com>
2026-06-16 16:11:27 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# HTML fayllar uchun yo'l
+# HTML fayllar turgan papkani ulash
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
